8f8b4ce208a185f56ea1b30a3ed1dc13941af38f
FlutLab.io <>
2022-05-03 14:18:40 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/torres/com/MainActivity.kt
@@ -0,0 +1,6 @@
+package torres.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ConsultorioApp());
+} // funcion principal
+
+class ConsultorioApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App consultorio dental',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: PaginaInicial());
+  } //widget
+} //class widgets sin estado
+
+class PaginaInicial extends StatefulWidget {
+  PaginaInicial({Key? key}) : super(key: key);
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+} //widget con estado
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  List<String> images = [
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+    "assets/images/consultorio1.jpeg",
+    "assets/images/consultorio4.jpg",
+    "assets/images/consultorio5.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Tutorial GridView"),
+        ),
+        body: GridView.builder(
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(images[index], fit: BoxFit.cover);
+          },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
